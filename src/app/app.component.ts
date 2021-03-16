919aa2a6cd3bfb64baf5a7684498f65ced9514bc
daniel <danieljustino79@gmail.com>
2021-03-16 16:41:56 -0300

    Primeng install, config  primeFlex, menubar.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {MenuItem} from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pocket-post-frontend-prime';
+  items: MenuItem[];
+
+  constructor() { 
+    this.items = [];
+ }
+
+  ngOnInit() {
+    this.items = [
+        {
+            label: 'Publications',
+            icon: 'pi pi-book'
+        }
+    ];
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+
+import {MenubarModule} from 'primeng/menubar';
+import { SharedModule } from 'primeng/api';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    MenubarModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
